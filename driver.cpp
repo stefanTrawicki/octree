eafc45e14a3a8db2790b7111195fe893302ff251
Stefan Trawicki <s.trawicki@lancaster.ac.uk>
2021-09-29 20:33:48 +0100

    ...

--- driver.cpp
+++ driver.cpp
@@ -3,9 +3,9 @@
 #include <unistd.h>
 #include <chrono>
 
-#define CONT_SIZE 10000
-#define TEST_SET_SIZE 9200
-#define BOX_SIZE 3
+#define CONT_SIZE 100
+#define TEST_SET_SIZE 1000000
+#define LAYERS 4
 #define RAND_MAC static_cast <double> (rand()) / (static_cast <double> (RAND_MAX/CONT_SIZE))
 
 int main(int argc, char const *argv[])
@@ -13,69 +13,37 @@ int main(int argc, char const *argv[])
 
     srand(time(0));
 
-    OVector3 origin = OVector3{0, 0, 0};
-    OVector3 bounds = OVector3{CONT_SIZE, CONT_SIZE, CONT_SIZE};
+    QVector2 origin = QVector2{0, 0};
+    QVector2 bounds = QVector2{CONT_SIZE, CONT_SIZE};
 
-    Octree<int> o = Octree<int>(origin, bounds, 5);
+    Quadtree<int> o = Quadtree<int>(origin, bounds, LAYERS);
 
-    std::vector<OVector3> item_list_test = std::vector<OVector3>(0);
+    std::vector<struct Storable<int>> linear_test = std::vector<struct Storable<int>>(0);
 
-    for (size_t i = 0; i < TEST_SET_SIZE; i++)
+    for (int i = 0; i < TEST_SET_SIZE; i++)
     {
-        OVector3 pos = {RAND_MAC, RAND_MAC, RAND_MAC};
-        o.Put(1, pos);
-        item_list_test.push_back(pos);
+        QVector2 v = {RAND_MAC, RAND_MAC};
+        if (!o.Put(i, v))
+            std::cout << "I failed somewhere! (" << v << ")" << std::endl; 
+        struct Storable<int> s = {i, v};
+        linear_test.push_back(s);
     }
 
-    std::vector<struct Storable<int>> results = std::vector<struct Storable<int>>(0);
+    QVector2 lower = {0,0};
+    QVector2 upper = {10,10};
 
-    OVector3 ret = {RAND_MAC, RAND_MAC, RAND_MAC};
-    OVector3 ret_end = {ret.x+BOX_SIZE, ret.y+BOX_SIZE, ret.z+BOX_SIZE};
+    std::vector<struct Storable<int> *> results = o.Get(lower, upper);
+    std::vector<struct Storable<int>> results_linear = std::vector<struct Storable<int>>(0);
 
-    std::vector<OVector3> results_2 = std::vector<OVector3>(0);
-    std::vector<OVector3> results_3 = std::vector<OVector3>(0);
-
-    auto start_manual = std::chrono::high_resolution_clock::now();
-
-    for (auto const& i : item_list_test)
+    for (size_t j = 0; j < linear_test.size(); j++)
     {
-        // std::cout << "i: " << i << " ret: " << ret << " i > ret " << (i > ret) << std::endl;
-        if (i > ret && i < ret_end)
-        {
-            results_2.push_back(i);
-        }
+        struct Storable<int> i = linear_test.at(j);
+        if (i.position >= lower && i.position < upper)
+            results_linear.push_back(i);
     }
 
-    auto finish_manual = std::chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(finish_manual-start_manual).count() << "ns\n";
-
-    auto start_octree = std::chrono::high_resolution_clock::now();
-
-    size_t drumroll_pls = o.GetFromArea(
-        ret,
-        ret_end,
-        &results
-    );
-
-
-    for (auto const& i : results)
-    {
-        // std::cout << "i: " << i << " ret: " << ret << " i > ret " << (i > ret) << std::endl;
-        if (i.position > ret && i.position < ret_end)
-        {
-            results_3.push_back(i.position);
-        }
-    }
-    auto finish_octree = std::chrono::high_resolution_clock::now();
-
-    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(finish_octree-start_manual).count() << "ns\n";
-
-    // std::cout << o << std::endl;
-
-    std::cout << drumroll_pls << std::endl;
-    std::cout << results.size() << std::endl;
-    std::cout << results_2.size() << std::endl;
-    std::cout << results_3.size() << std::endl;
+    std::cout << "results: " << results.size() << std::endl;
+    std::cout << "linear search: " << results_linear.size() << std::endl;
 
     return 0;
 }
--- src/Octree.hpp
+++ src/Octree.hpp
@@ -1,73 +1,96 @@
-#ifndef OCTREE_HPP
-#define OCTREE_HPP
+#ifndef QUADTREE_HPP
+#define QUADTREE_HPP
 
 #include <iostream>
 #include <vector>
+#include <math.h>
 
-class OVector3
+class QVector2
 {
 public:
     double x;
-    double y;
     double z;
 
-    friend bool operator==(const OVector3 &l, const OVector3 &r);
-    friend bool operator<(const OVector3 &l, const OVector3 &r);
-    friend bool operator<=(const OVector3 &l, const OVector3 &r);
-    friend bool operator>(const OVector3 &l, const OVector3 &r);
-    friend bool operator>=(const OVector3 &l, const OVector3 &r);
-    friend bool operator!=(const OVector3 &l, const OVector3 &r);
-    friend std::ostream &operator<<(std::ostream &os, const OVector3 &o);
-    OVector3 &operator+=(const OVector3 &rhs);
-    friend OVector3 operator+(OVector3 lhs, const OVector3 &rhs);
-    void OVec3Mult(float factor);
-};
+    friend bool operator==(const QVector2 &l, const QVector2 &r)
+    {
+        return (l.x == r.x && l.z == r.z);
+    }
 
-bool operator==(const OVector3 &l, const OVector3 &r);
-bool operator<(const OVector3 &l, const OVector3 &r);
-bool operator<=(const OVector3 &l, const OVector3 &r);
-bool operator>(const OVector3 &l, const OVector3 &r);
-bool operator>=(const OVector3 &l, const OVector3 &r);
-bool operator!=(const OVector3 &l, const OVector3 &r);
-std::ostream &operator<<(std::ostream &os, const OVector3 &o);
-OVector3 operator+(OVector3 lhs, const OVector3 &rhs);
-void OVec3Mult(float factor);
+    friend bool operator<(const QVector2 &l, const QVector2 &r)
+    {
+        return (l.x < r.x && l.z < r.z);
+    }
+    friend bool operator<=(const QVector2 &l, const QVector2 &r)
+    {
+        return (l < r || l == r);
+    }
+    friend bool operator>(const QVector2 &l, const QVector2 &r)
+    {
+        return (l.x > r.x && l.z > r.z);
+    }
+    friend bool operator>=(const QVector2 &l, const QVector2 &r)
+    {
+        return (l > r || l == r);
+    }
+    friend bool operator!=(const QVector2 &l, const QVector2 &r)
+    {
+        return !(l == r);
+    }
+    friend std::ostream &operator<<(std::ostream &os, const QVector2 &o)
+    {
+        os << "{" << o.x << ", " << o.z << "}";
+        return os;
+    }
+    friend QVector2 operator+(QVector2 lhs, const QVector2 &rhs)
+    {
+        lhs += rhs;
+        return lhs;
+    }
+    QVector2 &operator+=(const QVector2 &rhs)
+    {
+        x += rhs.x;
+        z += rhs.z;
+        return *this;
+    }
+    QVector2 &operator*=(const QVector2 &rhs)
+    {
+        x *= rhs.x;
+        z *= rhs.z;
+        return *this;
+    }
+};
 
 template <class T>
 struct Storable
 {
     T data;
-    OVector3 position;
+    QVector2 position;
 };
 
 template <class T>
-class Octree;
+class Quadtree;
 
-class OctreeCell
+template <class T>
+class QuadtreeCell
 {
 private:
-    std::vector<size_t> *items_index;
-    OctreeCell **neighbours;
-    OctreeCell *parent;
-    OctreeCell *children[8];
+    QuadtreeCell *neighbours[2];
+    QuadtreeCell *parent;
+    QuadtreeCell *children[4];
     unsigned short index;
-    size_t layer;
-
-    size_t GetLayer();
-    void SetNeighbour(unsigned short direction, OctreeCell *cell);
 
 public:
-    OctreeCell(bool isLeaf, OctreeCell *parent, unsigned short index);
+    QuadtreeCell(bool isLeaf, QuadtreeCell *parent, unsigned short index);
     void Subdivide(size_t layer);
     void Link(size_t layer);
-    bool IsLeaf();
     size_t Size();
-    OctreeCell *GetChildren(unsigned short index);
-    unsigned short GetIndex();
-    OctreeCell* GetNeighbour(unsigned short index);
-    std::vector<size_t> *GetIndexContainer();
+    bool IsLeaf();
+    QuadtreeCell *GetChildren(unsigned short index);
+    QuadtreeCell* GetNeighbour(unsigned short index);
+    std::vector<struct Storable<T> *> *items;
+    void SetNeighbour(unsigned short direction, QuadtreeCell *cell);
 
-    friend std::ostream &operator<<(std::ostream &os, const OctreeCell &o)
+    friend std::ostream &operator<<(std::ostream &os, const QuadtreeCell &o)
     {
         os << "This: " << &o << std::endl;
         os << "Index: " << o.index << std::endl;
@@ -83,7 +106,6 @@ public:
             }
         }
         std::cout << "Parent: " << o.parent << std::endl;
-        std::cout << "Item index: " << o.items_index->size() << std::endl;
         return os;
     }
 };
@@ -91,95 +113,71 @@ public:
 // generics can be a bit funny about having separate defintion and implementations
 
 template <class T>
-class Octree
+class Quadtree
 {
 private:
-    OVector3 origin;
-    OVector3 bounds;
+    QVector2 origin;
+    QVector2 bounds;
     size_t n_layers;
     size_t n_containers;
-    std::vector<struct Storable<T>> items;
-    OctreeCell *root;
-
-    inline void SpaceReduce(OVector3 &origin, OVector3 &bounds, unsigned short index);
+    QuadtreeCell<T> *root;
 
 public:
-    Octree<T>(OVector3 origin, OVector3 bounds, size_t n_layers);
-    ~Octree<T>();
-    OctreeCell *FindContainer(OVector3 target);
-    std::vector<T *> GetFromPosition(OVector3 target);
-    bool Put(T data, OVector3 position);
-    size_t Size();
-    bool IsStorable(OVector3 target);
-    size_t GetFromArea(OVector3 lower_bound, OVector3 upper_bound, std::vector<struct Storable<T>> *results);
+    Quadtree<T>(QVector2 origin, QVector2 bounds, size_t n_layers);
+    QuadtreeCell<T> *FindContainer(QVector2 target);
+    bool Put(T data, QVector2 position);
+    inline bool IsStorable(QVector2 target);
+    std::vector<struct Storable<T> *> Get(QVector2 lower_bound, QVector2 upper_bound);
 
-    friend std::ostream &operator<<(std::ostream &os, const Octree<T> &o)
+    friend std::ostream &operator<<(std::ostream &os, const Quadtree<T> &o)
     {
         os << "origin: " << o.origin << std::endl;
         os << "bounds: " << o.bounds << std::endl;
         os << "n_layers: " << o.n_layers << std::endl;
         os << "n_containers: " << o.n_containers << std::endl;
-        os << "n_items: " << o.items.size();
         return os;
     }
 };
 
 template <class T>
-bool Octree<T>::IsStorable(OVector3 target)
+inline bool Quadtree<T>::IsStorable(QVector2 target)
 {
     return !(target < origin || target >= (origin + bounds));
 }
 
 template <class T>
-Octree<T>::Octree(OVector3 origin, OVector3 bounds, size_t n_layers) : origin(origin), bounds(bounds), n_layers(n_layers)
+Quadtree<T>::Quadtree(QVector2 origin, QVector2 bounds, size_t n_layers) : origin(origin), bounds(bounds), n_layers(n_layers)
 {
-    root = new OctreeCell(n_layers == 0, root, 0);
+    root = new QuadtreeCell<T>(n_layers == 0, root, 0);
     root->Subdivide(n_layers);
     root->Link(n_layers);
-    n_containers = 8;
-    for (size_t i = 0; i < n_layers; i++)
-        n_containers *= 8;
-    items = std::vector<struct Storable<T>>();
+    n_containers = pow(4, n_layers);
 }
 
 template <class T>
-Octree<T>::~Octree()
-{
-    delete (root);
-}
-
-template <class T>
-inline void Octree<T>::SpaceReduce(OVector3 &origin, OVector3 &bounds, unsigned short index)
-{
-    bounds.OVec3Mult(0.5);
-    origin.x += ((index >> 2) % 2 != 0) ? bounds.x : 0;
-    origin.y += ((index >> 1) % 2 != 0) ? bounds.y : 0;
-    origin.z += ((index >> 0) % 2 != 0) ? bounds.z : 0;
-}
-
-template <class T>
-OctreeCell *Octree<T>::FindContainer(OVector3 target)
+QuadtreeCell<T> *Quadtree<T>::FindContainer(QVector2 target)
 {
     if (!IsStorable(target))
     {
         return NULL;
     }
 
-    OVector3 cons_origin = origin;
-    OVector3 cons_bounds = bounds;
+    QVector2 cons_origin = origin;
+    QVector2 cons_bounds = bounds;
 
-    OctreeCell *cell = root;
+    QuadtreeCell<T> *cell = root;
     unsigned short index = 0;
 
     while (!cell->IsLeaf())
     {
-        index = (target.x > (cons_origin.x + (cons_bounds.x / 2.0f)));
+        cons_bounds *= QVector2{0.5, 0.5};
+        index = (target.x > (cons_origin.x + cons_bounds.x));
         index = index << 1;
-        index += (target.y > (cons_origin.y + (cons_bounds.y / 2.0f)));
-        index = index << 1;
-        index += (target.z > (cons_origin.z + (cons_bounds.z / 2.0f)));
+        index += (target.z > (cons_origin.z + cons_bounds.z));
 
-        SpaceReduce(cons_origin, cons_bounds, index);
+        cons_origin.x += ((index >> 1) % 2 != 0) ? cons_bounds.x : 0;
+        cons_origin.z += ((index >> 0) % 2 != 0) ? cons_bounds.z : 0;
+
         cell = cell->GetChildren(index);
     }
 
@@ -187,105 +185,168 @@ OctreeCell *Octree<T>::FindContainer(OVector3 target)
 }
 
 template <class T>
-std::vector<T *> Octree<T>::GetFromPosition(OVector3 target)
+bool Quadtree<T>::Put(T data, QVector2 position)
 {
-    std::vector<T *> found_items = std::vector<T *>(0);
-
-    OctreeCell *cell = FindContainer(target);
+    QuadtreeCell<T> *cell = FindContainer(position);
     if (cell)
     {
-        if (cell->IsLeaf())
-        {
-            for (size_t i = 0; i < cell->GetIndexContainer()->size(); i++)
-            {
-                size_t index = cell->GetIndexContainer()->at(i);
-                if (items.at(index).position == target)
-                    found_items.push_back(&(items.at(index).data));
-            }
-        }
-    }
-    return found_items;
-}
+        struct Storable<T> *store_this = (struct Storable<T> *)malloc(sizeof(struct Storable<T>));
+        store_this->data = data;
+        store_this->position = position;
 
-template <class T>
-size_t Octree<T>::Size()
-{
-    return items.size();
-}
+        if (!cell->items)
+            cell->items = new std::vector<struct Storable<T> *>;
 
-template <class T>
-bool Octree<T>::Put(T data, OVector3 position)
-{
-    OctreeCell *cell = FindContainer(position);
-    if (cell)
-    {
-        struct Storable<T> stored =
-        {
-            data, position
-        };
-        items.push_back(stored);
-        cell->GetIndexContainer()->push_back(Size() - 1);
+        cell->items->push_back(store_this);
+
         return true;
     }
     return false;
 }
 
 template <class T>
-size_t Octree<T>::GetFromArea(OVector3 lower_bound, OVector3 upper_bound, std::vector<struct Storable<T>> *results)
+std::vector<struct Storable<T> *> Quadtree<T>::Get(QVector2 lower_bound, QVector2 upper_bound)
 {
-    if (!(IsStorable(lower_bound) && IsStorable(upper_bound))) return 0;
+    if (!(IsStorable(lower_bound) || IsStorable(upper_bound))) return std::vector<struct Storable<T> *>(0);
     
-    if (upper_bound == lower_bound) return 0;
+    // will be a specific case to add later
+    // if (upper_bound == lower_bound) return 0;
+
     if (lower_bound > upper_bound)
     {
-        OVector3 temp = lower_bound;
+        QVector2 temp = lower_bound;
         lower_bound = upper_bound;
         upper_bound = temp;
     }
 
-    OctreeCell *points[4] = {NULL, NULL, NULL, NULL};
+    QuadtreeCell<T> *points[3] = {NULL, NULL, NULL};
 
     points[0] = FindContainer(lower_bound);
-    if (!points[0]) return 0;
+    if (!points[0]) return std::vector<struct Storable<T> *>(0);
 
     // in x dim
-    points[1] = FindContainer(OVector3{lower_bound.x + upper_bound.x, lower_bound.y, lower_bound.z});
-    // in y dim
-    points[2] = FindContainer(OVector3{lower_bound.x, lower_bound.y + upper_bound.y, lower_bound.z});
+    points[1] = FindContainer(QVector2{lower_bound.x + upper_bound.x, lower_bound.z});
     // in z dim
-    points[3] = FindContainer(OVector3{lower_bound.x, lower_bound.y, lower_bound.z + upper_bound.z});
+    points[2] = FindContainer(QVector2{lower_bound.x, lower_bound.z + upper_bound.z});
 
-    if (!points[1] || !points[2] || !points[3]) return 0;
+    if (!points[1] || !points[2]) return std::vector<struct Storable<T> *>(0);
 
-    OctreeCell *windows[3] = {points[0], points[0], points[0]};
+    QuadtreeCell<T> *windows[2] = {points[0], points[0]};
+
+    std::vector<struct Storable<T> *> results = std::vector<struct Storable<T> *>(0);
 
     size_t count = 0;
 
-    while(windows[2] != points[3]->GetNeighbour(2))
+    while(windows[1] != points[2]->GetNeighbour(1))
     {
-        while(windows[1] != points[2]->GetNeighbour(1))
+        while(windows[0] != points[1]->GetNeighbour(0))
         {
-            while(windows[0] != points[1]->GetNeighbour(0))
+            count++;
+            if (windows[0]->items)
             {
-                // std::cout << "\t\tx" << std::endl;
-                for (auto const& i : *(windows[0]->GetIndexContainer()))
+                for (struct Storable<T> *i : *(windows[0]->items))
                 {
-                    results->push_back(items.at(i));                    
+                    if (i->position >= lower_bound && i->position < upper_bound)
+                    {
+                        results.push_back(i);
+                    }
                 }
-                count++;
-                windows[0] = windows[0]->GetNeighbour(0);
             }
-            // std::cout << "\ty" << std::endl;
-            windows[0] = points[0];
-            windows[1] = windows[1]->GetNeighbour(1);
+            windows[0] = windows[0]->GetNeighbour(0);
         }
-        // std::cout << "z" << std::endl;
-        windows[1] = points[0];
-        windows[2] = windows[2]->GetNeighbour(2);
+        // std::cout << "\ty" << std::endl;
+        windows[0] = points[0];
+        windows[1] = windows[1]->GetNeighbour(1);
     }
 
-    return count;
+    return results;
 
 }
 
-#endif //OCTREE_HPP
+template <class T>
+QuadtreeCell<T>::QuadtreeCell(bool isLeaf, QuadtreeCell<T> *parent, unsigned short index) : parent(parent), index(index)
+{
+    children[0] = NULL;
+    neighbours[0] = NULL;
+}
+
+template <class T>
+bool QuadtreeCell<T>::IsLeaf()
+{
+    return children[0] == NULL;
+}
+
+template <class T>
+void QuadtreeCell<T>::Subdivide(size_t layer)
+{
+
+    if (layer > 0)
+    {
+        // creating children
+        for (unsigned short i = 0; i < 4; i++)
+        {
+            children[i] = new QuadtreeCell(layer - 1 == 0, this, i);
+            children[i]->Subdivide(layer - 1);
+        }
+    }
+}
+
+template <class T>
+void QuadtreeCell<T>::Link(size_t layer)
+{
+    for (unsigned short child_i = 0; child_i < 4; child_i++)
+    {
+        QuadtreeCell<T> *child = children[child_i];
+
+        for (unsigned short i = 0; i < 2; i++)
+        {
+            // given dimensions 0 = x, 1 = y, 2 = z
+            unsigned short neighbour = child->index & (2 >> i) ? child->index - (2 >> i) : child->index + (2 >> i);
+
+            // if the index cannot be increased in this dimension without expanding into other container
+            if ((child->index & (2 >> i)) > 0)
+            {
+                // std::cout << "will be remote!" << std::endl;
+                // is remote
+                if (neighbours[i])
+                {            
+                    child->neighbours[i] = neighbours[i]->children[neighbour%4];
+                }
+                else
+                {
+                    child->neighbours[i] = NULL;
+                }
+            }
+            else
+            {        
+                // is local
+                child->neighbours[i] = children[neighbour%4];
+            }
+            
+        }
+    }
+
+    if (layer > 1)
+    {
+        for (unsigned short child_i = 0; child_i < 4; child_i++)
+        {
+            children[child_i]->Link(layer-1);
+        }
+    }
+}
+
+template <class T>
+QuadtreeCell<T> *QuadtreeCell<T>::GetChildren(unsigned short index)
+{
+    return children[index];
+}
+
+template <class T>
+QuadtreeCell<T> *QuadtreeCell<T>::GetNeighbour(unsigned short index)
+{
+    if (index < 0 || index > 1) return NULL;
+
+    return neighbours[index];
+}
+
+#endif //Quadtree_HPP
